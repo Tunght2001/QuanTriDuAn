03e33e6411613ecaddaedc62143aac6fa611bf6f
Tunght2001 <thahtung01@gmail.com>
2022-06-09 23:39:57 +0700

    footer

--- inc/footer.php
+++ inc/footer.php
@@ -56,7 +56,7 @@
         <div class="row">
             <div class="col-md-8">
                 <div class="copyright">
-                    <p>&copy; 2015 uCommerce. All Rights Reserved. <a href="http://www.freshdesignweb.com" target="_blank">freshDesignweb.com</a></p>
+                    <p>&copy;Web PC</p>
                 </div>
             </div>
             <div class="col-md-4">
